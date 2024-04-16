2248f1872462f7d4c3fbe3003f5f34c68091e6a3
Emilahm999 <emilhika777@gmail.com>
2024-04-16 17:48:41 +0500

    add description block

---
+++ src/js/index/first-block.js
@@ -0,0 +1,11 @@
+// import ObserverClass from "../_classes/ObserverClass.js";
+// import { PopUp } from "../_classes/Pop-up.js";
+var swiper = new Swiper(".cta__swiper", {
+    pagination: {
+      el: ".cta__pagination",
+    },
+    navigation: {
+      nextEl: ".cta__arrow.next",
+      prevEl: ".cta__arrow.prev",
+    },
+  });
